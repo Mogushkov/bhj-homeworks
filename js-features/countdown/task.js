f0cf0801e9e32b2fcbc10d199e4fb50bf0a9eef4
Gapur Mogushkov <gapur@mail.ru>
2021-01-17 22:23:25 +0300

    countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,9 @@
+let timeCounter = setInterval(function () {
+    const timer = document.getElementById('timer');
+    timer.textContent--;
+
+    if(timer.textContent == 0) {
+        alert('Вы победили в конкурсе!');
+        clearInterval(timeCounter);
+    }
+}, 1000) ;
