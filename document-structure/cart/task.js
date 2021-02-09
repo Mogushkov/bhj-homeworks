942fd9a4412d56d931dab3129de685e7d9d949ac
Gapur Mogushkov <gapur@mail.ru>
2021-02-09 22:38:07 +0300

    исправление

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,7 +1,7 @@
 let products = document.querySelectorAll('.product'),
     cartProducts = document.querySelector('.cart__products');
 
-for (let i = 0; i <= products.length; i++) {
+for (let i = 0; i < products.length; i++) {
     quantityDec(i);
     quantityInc(i);
     addToCart(i);
@@ -17,7 +17,7 @@ function quantityInc(i) {
 function quantityDec(i) {
     products[i].querySelector('.product__quantity-control_dec').addEventListener('click', () => {
         let value = parseInt(products[i].querySelector('.product__quantity-value').innerText);
-        if (value >= 1) {
+        if (value > 1) {
             products[i].querySelector('.product__quantity-value').innerText = 
             parseInt(products[i].querySelector('.product__quantity-value').innerText) - 1;
         };
@@ -27,8 +27,8 @@ function quantityDec(i) {
 function addToCart(i) {
     products[i].querySelector('.product__add').addEventListener('click', () => {
         let cartProduct = Array.from(document.querySelectorAll('.cart__product'));
-        for (let j = 1; j < cartProduct.length; j++) {
-            if (cartProduct[j].dataset.id === products[i].dataset.id) {
+        for (let j = 0; j < cartProduct.length; j++) {
+            if (cartProduct[j].dataset.id == products[i].dataset.id) {
                 cartProduct[j].querySelector('.cart__product-count').innerText = 
                 parseInt(cartProduct[j].querySelector('.cart__product-count').innerText) + 
                 parseInt(products[i].querySelector('.product__quantity-value').innerText);
